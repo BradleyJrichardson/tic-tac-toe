e0c25acc0ba54b9677752ac67b0486b67b47f2d1
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-23 16:44:22 +1000

    added winning logic

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
 let counter = 1;
+var xArray = [];
+var oArray = [];
+const winningArrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
 
 const whoseTurn = object => {
   if (counter % 2 === 0) {
@@ -9,15 +21,17 @@ const whoseTurn = object => {
   } else {
     addO(object);
     counter++;
-    console.log(counter);
   }
 };
 
 const addX = object => {
-  alert(object);
   document.getElementById(object.id).innerHTML = "<div class='x'>X</div>";
+  xArray.push(parseInt(object.id));
+  console.log(xArray);
 };
+
 const addO = object => {
-  alert(object);
   document.getElementById(object.id).innerHTML = "<div class='x'>O</div>";
+  oArray.push(parseInt(object.id));
+  console.log(oArray);
 };
