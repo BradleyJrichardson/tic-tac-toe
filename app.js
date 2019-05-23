b6eaea7dd1c58ad567a079f4fe9d7b1be93dfadf
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-24 09:16:07 +1000

    changed testing framework to jest

---
+++ app.js
@@ -0,0 +1,53 @@
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
+
+module.exports = { sum, mul, sub, div };
+
+let board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+let counter = 1;
+var xArray = [];
+var oArray = [];
+const winningArrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
+
+const whoseTurn = object => {
+  if (counter % 2 === 0) {
+    addX(object);
+    counter++;
+  } else {
+    addO(object);
+    counter++;
+  }
+};
+
+const checkWinX = xArray => {
+  return winningArrays.forEach(array => {
+    return xArray.includes(array);
+  });
+};
+
+console.log(checkWinX([1, 3, 4, 2]));
+
+const addX = object => {
+  document.getElementById(object.id).innerHTML = "<div class='x'>X</div>";
+  xArray.push(parseInt(object.id));
+  if (checkWinX()) {
+    alert("winner");
+  }
+};
+
+const addO = object => {
+  document.getElementById(object.id).innerHTML = "<div class='x'>O</div>";
+  oArray.push(parseInt(object.id));
+};
--- test.js
+++
@@ -1,8 +0,0 @@
-const expect = require("mocha").expect;
-const server = require("../script");
-
-describe("test", () => {
-  it("should return a string", () => {
-    expect("ci with travis").to.equal("ci with travis");
-  });
-});
